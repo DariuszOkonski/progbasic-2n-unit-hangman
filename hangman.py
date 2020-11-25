7d2fb0ff30a833bc701d639cf5e4772dba3ae219
Dariusz <darek200180@gmail.com>
2020-11-25 22:01:54 +0000

    add display_current_state funcionality

--- hangman.py
+++ hangman.py
@@ -55,16 +55,39 @@ def get_word_to_quess():
 
 def change_word_to_list(word):
     return list(word)
+
+def initial_quess_state(word):
+    quess_state = []
+    for item in word:
+        if item == ' ':
+            quess_state.append(' ')
+        else:
+            quess_state.append('_')
+    return quess_state
+
+def display_current_state(word, lives):
+    print("Current Game State: ")
+    print("-------------------")
+    print("")
+    print(f"Lives left: {lives}")
+    print(' '.join(word))
+    print("")
+
 # =========================================
 
 def play(word, lives = 7):
     clear_console()
 
-
     word = change_word_to_list(word)
+    guess_state = initial_quess_state(word)
 
-    print(type(word), word, len(word))
-    print(type(lives), lives)
+    display_current_state(guess_state, lives)
+
+    # print(word)
+    # print(guess_state)
+
+    # print(type(word), word, len(word))
+    # print(type(lives), lives)
     pass
 
 
@@ -73,6 +96,7 @@ def run_game():
     # word = get_word_to_quess()
     # play(word, lives)
     play('Hong Kong', 5)
+    # play('Polska', 5)
 
 # ==========================================
 
