480921e46c3d800bccd9c512b1252ed56ed7deef
Dariusz Okonski <darek200180@gmail.com>
2020-11-25 12:21:50 +0000

    listed game requirements

--- hangman.py
+++ hangman.py
@@ -1,7 +1,39 @@
+# levels 
+# 7 very easy
+# 6 easy
+# 5 medium
+# 4 hard
+# 3 very hard
+
+
+# - wyswietlić menu programu
+# - zwrócić poziom od 7 do 3 do zmiennej LIVES
+# - odczytać plik i zwrócić losowe państwo do zmiennej WORLD
+
+# - ==================  dalej wszystko będzie się działo w funkcji PLAY
+# - wyswietlić initial state of game _ _ _ _ _ oraz liczbę żyć
+# - pobrać literę od użytkownika z zakresu a-z A-Z
+
+# - sprawdzić czy litera znajduje się w zmiennej WORLD: 
+#       jeśli tak to wyświetlić stan np _ a r s _ _
+#       jeśli nie to zabrać jedno życie
+#       jeśli podana litera była już wciśnięta to zawiadomić o tym użytkownika
+
+# - sprawdzić czy koniec gry:
+#       jeśli użytkownik nie ma już żyć to konieć gry i wyświetlić mu szukane hasło
+#       jeśli odgadł wszystkie to pogratulować wygranej
+# 
+# - zapytać czy grać raz jeszcze:
+#       jeśli tak to wyświetlić ponownie poziomy
+#       jeśli nie to opuścić grę      
+
+
+
+
+
 def play(word, lives):
     pass
 
 
 play('Codecool', 6)
 
-print("Hangman")
